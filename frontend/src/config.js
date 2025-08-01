bbbf54fa175a13f9d4ec615e33c4091d0ba92f75
Darkpwd <natanyuri1010@gmail.com>
2025-07-31 21:48:44 -0300

    Initial commit: add frontend and backend structure

---
+++ frontend/src/config.js
@@ -0,0 +1,20 @@
+// replace below firebase config with your own
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAycWfssNxyDPHbkJ7pZ5_rp-Mz2Wwm5wI",
+  authDomain: "netflix-clone-e77fb.firebaseapp.com",
+  projectId: "netflix-clone-e77fb",
+  storageBucket: "netflix-clone-e77fb.firebasestorage.app",
+  messagingSenderId: "4762552220",
+  appId: "1:4762552220:web:55fa8003b89c97e3e92a59",
+  measurementId: "G-DDSD6PBQQB"
+};
+
+
+// craete a/c on themoviedb.org and replace access token below
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YTQ5OTZkNzJlMjUwNWE1NmYzOGIxMzM1ZjUyMTM2NiIsIm5iZiI6MTc1Mzk3MzQ2Mi41LCJzdWIiOiI2ODhiODJkNjIzZDg1YmEwNzIyYTVlYzEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.S4SJxY8LseNEHD2vaT7gSgb86NXPFwp9tKTcuVud3N0";
+
+
+  export {firebaseConfig, TMDB_Access_Key};
